5c7d937c7c0c7d8a046fe61c56f487412aa5bcfd
hankhank10 <m.a.hankinson@gmail.com>
2020-05-06 16:08:04 +0100

    Update usersettings.py

--- usersettings.py
+++ usersettings.py
@@ -9,7 +9,3 @@ sonosroom="Dining Room"
 
 #send anonymous usage statistics
 sendanonymoususagestatistics="yes"
-
-#these dont need to be changed
-usagestatsurl="https://hooks.zapier.com/hooks/catch/7461201/or85nia/"
-appversion = "Alpha 0.1"
