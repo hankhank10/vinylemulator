05881416b5ee6806249fb552696b449b300508fe
hankhank10 <m.a.hankinson@gmail.com>
2020-06-08 17:53:45 +0100

    Update appsettings.py

--- appsettings.py
+++ appsettings.py
@@ -1,3 +1,3 @@
 #these dont need to be changed
 usagestatsurl="https://maker.ifttt.com/trigger/newlog/with/key/e-YuH5NYk23DutoxZRIMWzn6YEgzmyYfcMHy3T-40_e"
-appversion = "Alpha 0.4"
+appversion = "Alpha 0.5"
