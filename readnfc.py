e939563a1267b89aad1005bb5bb5bc8f55db2e01
hankhank10 <m.a.hankinson@gmail.com>
2020-07-04 07:50:19 +0100

    Allows room changing

--- readnfc.py
+++ readnfc.py
@@ -35,9 +35,15 @@ def touched(tag):
             if receivedtext.startswith ('command'):
                 servicetype = "command"
                 sonosinstruction = receivedtext[8:]
+            
+            if receivedtext.startswith ('room'):
+                servicetype = "room"
+                sonosroom_local = receivedtext[5:]
+                print ("Sonos room changed to " + sonosroom_local)
+                return True
 
             if servicetype == "":
-                print ("Service type not recognised. Tag text should begin spotify, tunein, command or apple/applemusic (experimental). (Case matters: lower case please)")
+                print ("Service type not recognised. Tag text should begin spotify, tunein, command, room or apple/applemusic (experimental). (Case matters: lower case please)")
                 if usersettings.sendanonymoususagestatistics == "yes":
                     r = requests.post(appsettings.usagestatsurl, data = {'time': time.time(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3': 'invalid service type sent'})
                 return True
@@ -45,12 +51,12 @@ def touched(tag):
             print ("Detected " + servicetype + " service request")
 
             #build the URL we want to request
-            urltoget = usersettings.sonoshttpaddress + "/" + usersettings.sonosroom + "/" + sonosinstruction
+            urltoget = usersettings.sonoshttpaddress + "/" + sonosroom_local + "/" + sonosinstruction
 
             #clear the queue for every service request type except commands
             if servicetype <> "command":
                 print ("Clearing Sonos queue")
-                r = requests.get(usersettings.sonoshttpaddress + "/" + usersettings.sonosroom + "/clearqueue")
+                r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/clearqueue")
 
             #use the request function to get the URL built previously, triggering the sonos
             print ("Fetching URL via HTTP: "+ urltoget)
@@ -80,6 +86,10 @@ def touched(tag):
 print("Setting up reader...")
 reader = nfc.ContactlessFrontend('usb')
 print(reader)
+
+sonosroom_local = usersettings.sonosroom
+print ("Sonos room set to " + sonosroom_local)
+
 print("Ready!")
 print("")
 
