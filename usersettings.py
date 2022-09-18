3fb955cc189eef7a07a0cc7e05fdfc9c358db3a7
hankhank10 <m.a.hankinson@gmail.com>
2022-09-18 14:18:03 +0100

    Update usersettings.py

--- usersettings.py
+++ usersettings.py
@@ -2,7 +2,7 @@
 #if this is running on the local Raspberry Pi then localhost will work
 #if not, then set an IP address
 #eg  sonoshttpaddress="http://192.168.4.102:5005"
-sonoshttpaddress="http://192.168.4.36:5005"
+sonoshttpaddress="http://localhost:5005"
 
 #set the name of the Sonos room you want to play the music in
 sonosroom="Garden"
